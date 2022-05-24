4f5ab8c92182bf9065720aa6603a6e465a65ec8f
Mithun Adhikari <mithunadhikari40@gmail.com>
2022-05-24 16:24:36 +0545

    Ports changed

--- Frontend/src/components/GeneralScreens/Header.js
+++ Frontend/src/components/GeneralScreens/Header.js
@@ -1,103 +1,107 @@
-import React, { useState, useEffect, useContext } from 'react';
+import React, { useState, useEffect, useContext } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import SearchForm from './SearchForm';
-import '../../Css/Header.css'
-import { RiPencilFill } from 'react-icons/ri'
-import { FaUserEdit } from 'react-icons/fa'
-import { BiLogOut } from 'react-icons/bi'
-import { BsBookmarks } from 'react-icons/bs'
-import SkeletonElement from '../Skeletons/SkeletonElement';
-import { AuthContext } from '../../Context/AuthContext';
+import SearchForm from "./SearchForm";
+import "../../Css/Header.css";
+import { RiPencilFill } from "react-icons/ri";
+import { FaUserEdit } from "react-icons/fa";
+import { BiLogOut } from "react-icons/bi";
+import { FcHome } from "react-icons/fc";
 
+import { BsBookmarks } from "react-icons/bs";
+import SkeletonElement from "../Skeletons/SkeletonElement";
+import { AuthContext } from "../../Context/AuthContext";
 const Header = () => {
-    const bool = localStorage.getItem("authToken") ? true : false
-    const [auth, setAuth] = useState(bool)
-    const { activeUser } = useContext(AuthContext)
-    const [loading, setLoading] = useState(true)
-    const navigate = useNavigate()
+  const bool = localStorage.getItem("authToken") ? true : false;
+  const [auth, setAuth] = useState(bool);
+  const { activeUser } = useContext(AuthContext);
+  const [loading, setLoading] = useState(true);
+  const navigate = useNavigate();
 
-    useEffect(() => {
+  useEffect(() => {
+    setAuth(bool);
+    setTimeout(() => {
+      setLoading(false);
+    }, 1600);
+  }, [bool]);
 
-        setAuth(bool)
-        setTimeout(() => {
-            setLoading(false)
-        }, 1600)
+  const handleLogout = () => {
+    localStorage.removeItem("authToken");
+    navigate("/");
+  };
 
-    }, [bool])
+  return (
+    <div>
+      <header>
+        <div className="averager">
+            
+        {activeUser?.role !== "admin" ?  <Link className="readList-link" to="#">
+              {''}
+            <FcHome /> <h5>Portfolio</h5>
+            {''}
+          </Link> :<div/>}
 
+          <Link to="/" className="logo">
+            <h5>MITHUN'S BLOG</h5>
+          </Link>
 
-    const handleLogout = () => {
-        localStorage.removeItem("authToken");
-        navigate('/')
-    };
+          <SearchForm />
+          <div className="header_options">
+            {auth ? (
+              <div className="auth_options">
+                {activeUser.role === "admin" ? (
+                  <Link className="addStory-link" to="/addstory">
+                    <RiPencilFill /> Add Story{" "}
+                  </Link>
+                ) : (
+                  <div />
+                )}
 
-    return (
-
-        <header>
-            <div className="averager">
-
-                <Link to="/" className="logo">
-                    <h5>
-                        MERN BLOG
-
-                    </h5>
+                <Link to="/readList" className="readList-link">
+                  <BsBookmarks />
+                  <span id="readListLength">{activeUser.readListLength}</span>
                 </Link>
-                <SearchForm />
-                <div className='header_options'>
+                <div className="header-profile-wrapper ">
+                  {loading ? (
+                    <SkeletonElement type="minsize-avatar" />
+                  ) : (
+                    <img
+                      src={`/userPhotos/${activeUser.photo}`}
+                      alt={activeUser.username}
+                    />
+                  )}
 
-                    {auth ?
-                        <div className="auth_options">
+                  <div className="sub-profile-wrap  ">
+                    <Link className="profile-link" to="/profile">
+                      {" "}
+                      <FaUserEdit /> Profile{" "}
+                    </Link>
 
-
-                            <Link className='addStory-link' to="/addstory"><RiPencilFill /> Add Story </Link>
-
-
-                            <Link to="/readList" className='readList-link'>
-                                <BsBookmarks />
-                                <span id="readListLength">
-                                    {activeUser.readListLength}
-                                </span>
-                            </Link>
-                            <div className='header-profile-wrapper '>
-
-
-                                {loading ? <SkeletonElement type="minsize-avatar" />
-
-                                    :
-
-                                    <img src={`/userPhotos/${activeUser.photo}`} alt={activeUser.username} />
-
-                                }
-
-
-                                <div className="sub-profile-wrap  ">
-                                    <Link className='profile-link' to="/profile"  > <FaUserEdit />  Profile </Link>
-
-                                    <button className='logout-btn' onClick={handleLogout}> <BiLogOut />  Logout</button>
-
-                                </div>
-
-                            </div>
-
-
-                        </div>
-
-                        :
-                        <div className="noAuth_options">
-
-                            <Link className='login-link' to="/login"> Login </Link>
-
-                            <Link className='register-link' to="/register"> Get Started</Link>
-                        </div>
-
-                    }
+                    <button className="logout-btn" onClick={handleLogout}>
+                      {" "}
+                      <BiLogOut /> Logout
+                    </button>
+                  </div>
                 </div>
+              </div>
+            ) : (
+              <div className="noAuth_options">
+                <Link className="login-link" to="/login">
+                  {" "}
+                  Login{" "}
+                </Link>
 
-            </div>
-
-        </header>
-
-    )
-}
+                <Link className="register-link" to="/register">
+                  {" "}
+                  Get Started
+                </Link>
+              </div>
+            )}
+          </div>
+        </div>
+      </header>
+      <SearchForm formId="bottom-search" />
+    </div>
+  );
+};
 
 export default Header;
--- Frontend/src/components/ProfileScreens/ChangePassword.js
+++ Frontend/src/components/ProfileScreens/ChangePassword.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
-// import '../../Css/ChangePassword.css'
+import '../../Css/ChangePassword.css'
 import { IoEyeOffOutline, IoEyeOutline } from 'react-icons/io5'
 
 const ChangePassword = () => {
